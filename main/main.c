78df6db37ba42bc0dcba6835848852e400ddfa31
VfrewqazxcV <VfrewqazxcV@gmail.com>
2023-02-26 17:15:05 +0100

    Read all can cominikats

--- main/main.c
+++ main/main.c
@@ -3,19 +3,6 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 
-void can_receive_task(void *arg){
-    twai_message_t message;
-    for(;;){
-        if(twai_receive(&message, pdMS_TO_TICKS(1000)) == ESP_OK){
-            printf("Odebrano ramke CAN Data: " + message.identifier + message.data_length_code);
-            for(int i = 0; i < message.data_length_code; i++){
-                printf("%02X ", message.data[i]);
-            }
-            printf("\n");
-        }
-    }
-}
-
 void uart_init(){
     //Initialize configuration structures using macro initializers
     twai_general_config_t g_config = TWAI_GENERAL_CONFIG_DEFAULT(GPIO_NUM_17, GPIO_NUM_16, TWAI_MODE_LISTEN_ONLY);
@@ -29,8 +16,7 @@ void uart_init(){
         printf("Failed to install driver\n");
         return;
     }
-
-    //Start TWAI driver
+        //Start TWAI driver
     if (twai_start() == ESP_OK) {
         printf("Driver started\n");
     } else {
@@ -39,11 +25,34 @@ void uart_init(){
     }
 }
 
+void can_receive_task(){
+    twai_message_t message;
+        if(twai_receive(&message, pdMS_TO_TICKS(100)) == ESP_OK){
+            printf("Typ     : %lu", message.identifier);
+            printf("\n");
+            printf("Typ     : %03lX", message.identifier);
+            printf("\n");
+            printf("Dlugosc : %u", message.data_length_code);
+            printf("\n");
+            printf("Wartosc :");
+            for(int i = 0; i < message.data_length_code; i++){
+                printf(" %03u", message.data[i]);
+            }
+            printf("\n");
+            printf("Wartosc :");
+            for(int i = 0; i < message.data_length_code; i++){
+                printf(" %02X", message.data[i]);
+            }
+            printf("\n");
+        }
+        printf("\n");
+        vTaskDelete(NULL);
+}
+
 void app_main(void){
     uart_init();
-    xTaskCreate(&can_receive_task, "can_receive_task", 4096, NULL, 5, NULL);
     while(1){
-        printf("Czekamy na wiadomosc\n");
-        vTaskDelay(pdMS_TO_TICKS(3000));
+        xTaskCreate(&can_receive_task, "can_receive_task", 4096, NULL, 5, NULL);
+        vTaskDelay(pdMS_TO_TICKS(100));
     }
 }
